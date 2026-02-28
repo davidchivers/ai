5fce27cebc74e74f15c521e23ec2ae42ade3a708
Dave Chivers <dave_chivers@DESKTOP-HHDBOQL.localdomain>
2026-02-28 11:03:41 +0000

    Archive legacy Tuition_Fees files under 04_tuition_fees old folder

--- projects/02_Nimbyism_and_Housing_Supply/Code/Codes_ABB/wqregk_pt_age.m
+++
@@ -1,7 +0,0 @@
-function Obj = wqregk_pt_age(c)
-global tau Matdraw_t Matdraw_lag 
-
-Obj=mean((Matdraw_t-Matdraw_lag*c).*...
-    (tau-(Matdraw_t-Matdraw_lag*c<0)));
-    
-end
--- projects/02_Nimbyism_and_Housing_Supply/Code/Codes_ABB/wqregk_zeta_age.m
+++
@@ -1,7 +0,0 @@
-function Obj = wqregk_zeta_age(c)
-global tau Matdraw MatAGE1_tot T
-
-Obj=mean((Matdraw(:,T+1)-MatAGE1_tot*c).*...
-    (tau-(Matdraw(:,T+1)-MatAGE1_tot*c<0)));
-    
-end
